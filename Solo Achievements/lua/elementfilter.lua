ddf105d675fd68379dc9f45f2dda9e9c148a6874
shinrax2 <shinra.ubu1@yahoo.de>
2019-01-16 16:57:16 +0100

    Update elementfilter.lua
    fix potential crash

--- Solo Achievements/lua/elementfilter.lua
+++ Solo Achievements/lua/elementfilter.lua
@@ -1,9 +1,9 @@
 local orig_func = ElementFilter:_check_players
-function ElementFilter:_check_players(...)
+function ElementFilter:_check_players(self, ...)
 	-- for "Tag, You're it!"/ Car Shop
 	if managers.job:current_level_id() == "cage" then
 		return true
 	else
-		orig_func(...)
+		orig_func(self, ...)
 	end
 end
