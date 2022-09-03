2069c67018b170cfbfccee225612544523eeb733
Adamasnaldo <afonsogbernardo3@gmail.com>
2022-09-03 17:26:08 +0100

    Added Bring Your Cop To Work Day (ovk_9)

--- Solo Achievements/lua/coreelementcounter.lua
+++ Solo Achievements/lua/coreelementcounter.lua
@@ -1,16 +1,15 @@
 core:module("CoreElementCounter")
 
-Hooks:PostHook(ElementCounter, "_check_triggers", "ElementCounter__check_triggers_sa", function(self, type)
+Hooks:PreHook(ElementCounter, "_check_triggers", "ElementCounter__check_triggers_sa", function (self, type)
 	if not self._triggers[type] then
 		return
 	end
 
-	if managers.job:current_level_id() == "alex_3" and self._editor_name == "7bagsSecured" then
+	if managers.job:current_level_id() == "alex_3" and tostring(self:id()) == "100423" then -- 7bagsSecured
 		--Short Fuse
 		local num_players = managers.network:session():amount_of_players()
 		local num_bags = math.min(7, 3*num_players)
-		if type ~= "value" or self._values.counter_target == num_bags then
-			self._triggers[type][100827].callback()
-		end
+
+		self._triggers[type][100827].amount = num_bags
 	end
 end)
--- Solo Achievements/lua/coremissionmanager.lua
+++ Solo Achievements/lua/coremissionmanager.lua
@@ -1,15 +1,15 @@
 core:module("CoreMissionManager")
 
-Hooks:PreHook(MissionManager, "_add_script", "MissionManager__add_script_sa", function(self, data)
+Hooks:PreHook(MissionManager, "_add_script", "MissionManager__add_script_sa", function (self, data)
 	--Pass the Ammo
 	if managers.job:current_level_id() ~= "spa" then
 		return
 	end
 
 	for k, v in pairs(data.elements) do
-		if v.editor_name == "logic_counter_012" then
+		if tostring(v.id) == "102000" then -- logic_counter_012
 			v.values.counter_target = 2 + 2 * managers.network:session():amount_of_players()
-			return
+			break
 		end
 	end
 end)
